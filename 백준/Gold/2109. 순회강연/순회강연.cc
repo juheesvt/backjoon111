ba6f7199165b8638679e16a5e2bf14487a2e0930
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2022-07-04 14:29:36 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -12,25 +12,24 @@ int main() {
   cin.tie(nullptr);
 
   int n;
-  priority_queue<int, vector<int>,    
- greater<int>> pq; //최솟값이 루트로 가는 우선순위큐
-  vector<pair<int, int>> v; //d, p 저장 벡터
+  priority_queue<int, vector<int>, greater<int>> pq; //최솟값이 루트로 가는 우선순위큐
+  vector<pair<int, int>> univ; //d, p 저장 벡터
   int result = 0;
 
   cin >> n;
   for (int i = 0; i < n; i++) {
     int p, d;
     cin >> p >> d;
-    v.push_back(make_pair(d, p)); //day 순으로 정렬하기 위해 d, p로 저장
+    univ.emplace_back(d, p); //day 순으로 정렬
   }
  
-  sort(v.begin(), v.end());
+  sort(univ.begin(), univ.end());
  
   for (int i = 0; i < n; i++) {
-    pq.push(v[i].second);
-    result += v[i].second;
+    pq.push(univ[i].second);
+    result += univ[i].second;
  
-    if (pq.size() > v[i].first) {
+    if (pq.size() > univ[i].first) {
       result -= pq.top();
       pq.pop();
     }
