d796b708394d49312976d2f86ebf6c0d463be847
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2022-07-11 14:50:32 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> position(n, pair<int, int>(0, 0));
+
+    for (int i=0; i<n; i++)
+        cin >> position[i].first >> position[i].second;
+
+    // a 전봇대 기준 정렬
+    sort(position.begin(), position.end());
+
+    // b 전봇대에서 LIS 구하기
+    vector<int> lis (n, 0);
+    for (int i=0; i<n; i++) {
+        lis[i] = 1;
+        for (int j=0; j<i; j++) {
+            if (position[j].second < position[i].second) {
+                lis[i] = max(lis[i], lis[j]+1);
+            }
+        }
+    }
+
+    cout << n - *max_element(lis.begin(), lis.end());
+
+
+   return 0;
+}
