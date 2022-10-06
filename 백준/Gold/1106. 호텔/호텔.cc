6e36eeffd7eb2a42fda284113378d00c2fef2893
juheesvt <kangjuhee18@kookmin.ac.kr>
2022-10-06 22:06:31 +0900

    코드 변경

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,20 +1,56 @@
-#include <cstdio>
 #include <algorithm>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+
+
 using namespace std;
-int C,N,p[20],cost[20],dp[1001],inf=9e8;
-int main(){
-    scanf("%d%d",&C,&N);
-    for(int i=0;i<N;i++) scanf("%d%d",&cost[i],&p[i]);
-    for(int i=1;i<=C;i++) dp[i]=inf;
-    for(int i=0;i<N;i++){
-        for(int j=1;j<=C;j++){
-            for(int k=1;;k++){
-                int v=0;
-                if(j-p[i]*k>=0) v=dp[j-p[i]*k];
-                if(dp[j] > v+cost[i]*k) dp[j] = v+cost[i]*k;
-                else break;
+
+#define cost first
+#define client second
+
+int c, n;
+vector<pair<int, int>> city;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+    cin >> c >> n;
+
+    int tmp1, tmp2;
+    for (int i=0; i<n; i++) {
+        cin >> tmp1 >> tmp2;
+        city.emplace_back(tmp1, tmp2);
+    }
+
+    vector<int> cache (c+1, 0);
+    // 0번째 인덱스에 값 넣어주면 틀림 ~ ㅜㅜ
+    for (int i=1; i<c+1; i++)   cache[i] = 9e8;
+
+    // 0~N-1까지 반복문을 돌려주면서 i번 도시까지 홍보할때의 최솟값을 DP 배열에 순차적으로 저장
+    for (int i=0; i<n; i++) {
+        for (int j=1; j<c+1; j++) {
+            // 배수 역할
+            for (int k=1; ; k++) {
+                int value = 0;
+                if (j-city[i].client*k >= 0) {
+                    value = cache[j-city[i].client*k];
+                }
+                if (cache[j] > value + city[i].cost*k) {
+                    cache[j] = value + city[i].cost*k;
+                }
+                else {
+                    break;
+                }
             }
         }
     }
-    printf("%d",dp[C]);
+
+    cout << cache[c];
+
+    return 0;
 }
