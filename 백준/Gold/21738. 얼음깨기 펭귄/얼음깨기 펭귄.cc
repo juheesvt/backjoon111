6735f061bc2695dde9a8f97d61fa3f5669268f99
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2022-11-13 21:06:32 +0900

    [Gold V] Title: 얼음깨기 펭귄, Time: 172 ms, Memory: 25368 KB -BaekjoonHub

---
+++ 백준/Gold/21738. 얼음깨기 펭귄/얼음깨기 펭귄.cc
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, s, p, ans;
+vector<vector<int>> g;
+vector<bool> visit;
+
+int main() {
+	scanf("%d%d%d", &n, &s, &p);
+	g = vector<vector<int>>(n + 1);
+	priority_queue<int> pq;
+
+	for (int i = 0, a, b; i < n - 1; i++) {
+		scanf("%d%d", &a, &b);
+		g[a].push_back(b);
+		g[b].push_back(a);
+	}
+
+	visit = vector<bool>(n + 1, false);
+	for (int i = 1; i <= s; i++) {
+		queue<int> q;
+
+		q.push(i);
+		visit[i] = 1;
+
+		for (int time = 1, b = 1; b == 1; time++) {
+			int size = q.size();
+			while (size--) {
+				int cur = q.front(); q.pop();
+				if (cur == p) {
+					pq.push(-time); b = 0; visit[cur] = 0;
+					break;
+				}
+
+				for (auto x : g[cur]) {
+					if (!visit[x]) {
+						q.push(x); visit[x] = 1;
+					}
+				}
+
+			}
+		}
+	}
+	ans += -pq.top(); pq.pop();
+	ans += -pq.top(); pq.pop();
+	ans--;
+
+	printf("%d", n-ans);
+
+	return 0;
+}
