26bc01974abc12316697cb9a5d9dde48b83fd5c7
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-01-07 22:41:50 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -1,21 +1,14 @@
 #include <iostream>
 #include <string>
 #include <string.h>
-#define MAX 5001
-#define divider 1000000
- 
+
 using namespace std;
- 
+
+const int MAX = 5001;
+const int divider = 1000000;
+
 string code;
-int cache[MAX];
-int answer;
- 
-bool isZero(char c) {
-    if (c == '0') {
-        return true;
-    }
-    return false;
-}
+int DP[MAX];
  
 bool canChangeAlpha(string s) {
     int number = stoi(s);
@@ -33,36 +26,28 @@ int countDecode(string s) {
     }
  
     /*
-    점화식 : D[i] = D[i-1] + D[i-2]
-    ------------------------
-    D[i] : i번째 <까지> 해석될 수 있는 암호의 수
-    */
-    cache[0] = cache[1] = 1;
+     * 점화식 : DP[i] = DP[i-1] + DP[i-2]
+     * DP[i] : i번째 "까지" 해석될 수 있는 암호의 수
+     */
+    DP[0] = DP[1] = 1;
     for (int i = 2; i <= s.length(); i++) {
-        cache[i] = isZero(s[i-1]) ? 0 : cache[i - 1];
-        cache[i] += canChangeAlpha(s.substr(i - 2, 2)) ? cache[i - 2] : 0;
-        cache[i] %= divider;
+        DP[i] = s[i-1] == '0' ? 0 : DP[i - 1];
+        
+        // 알파벳 두개
+        DP[i] += canChangeAlpha(s.substr(i - 2, 2)) ? DP[i - 2] : 0;
+        DP[i] %= divider;
     }
  
-    return cache[s.length()];
+    return DP[s.length()];
 }
  
 int main() {
  
-    //초기화
     code = "";
-    memset(cache, -1, sizeof(cache));
-    answer = 0;
- 
-    //입력
+    memset(DP, -1, sizeof(DP));
+   
     cin >> code;
+ 	cout << countDecode(code);
  
-    //해법
-    answer = countDecode(code);
- 
-    //출력
-    cout << answer << "\n";
- 
-    //종료
     return 0;
 }
