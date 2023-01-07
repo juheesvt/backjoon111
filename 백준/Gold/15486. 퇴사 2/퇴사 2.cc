097254865dfebdff892c8dba6b4cb96b67648cb4
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-01-07 21:24:51 +0900

    [Gold V] Title: 퇴사 2, Time: 320 ms, Memory: 31676 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -3,11 +3,10 @@
  
 using namespace std;
  
-int d[1600000];
+int DP[1500000];
 vector<pair<int, int>> v;
 int n;
-int ans = 0;
- 
+
 int main() {
     scanf("%d", &n);
     v.push_back(make_pair(0, 0));
@@ -17,8 +16,8 @@ int main() {
         v.push_back(make_pair(x, y));
     }
     for (int i = 1; i <= n; i++) {
-        d[i+v[i].first] = max(d[i+v[i].first], d[i] + v[i].second);
-        d[i+1] = max(d[i+1], d[i]);
+        DP[i+v[i].first] = max(DP[i+v[i].first], DP[i] + v[i].second);
+        DP[i+1] = max(DP[i+1], DP[i]);
     }
-    printf("%d\n", d[n+1]);
+    printf("%d\n", DP[n+1]);
 }
