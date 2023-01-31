64acddef0dec137cf62e7ade8f9644953fa6988f
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2023-01-31 15:19:59 +0900

    [Gold II] Title: 빵집, Time: 148 ms, Memory: 27016 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.cc
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -1,52 +1,56 @@
-	#include <iostream>
-#include <string>
+#include <iostream>
+#include <cstring>
+#include <queue>
+
 using namespace std;
- 
-int r, c;
+
+
+int n, m;
+int answer = 0;
 string map[10000];
-bool visited[10000][500] = { false, };
-int ret = 0;
-const int dx[] = { 1,1,1 };
-const int dy[] = { -1,0,1 };
- 
- 
-bool dfs(int cy, int cx)
-{
-	visited[cy][cx] = true;
-	if (cx == c - 1)
-	{
-		ret++;
-		return true;
-	}
- 
-	for (int dir = 0; dir < 3; ++dir)
-	{
-		int nx = cx + dx[dir];
-		int ny = cy + dy[dir];
- 
-		if (ny < 0 || ny >= r || visited[ny][nx]|| map[ny][nx] == 'x')
-			continue;
- 
-		if (dfs(ny, nx))
-			return true;
-	}
-	return false;
+int visited[10000][500];
+
+// 오른쪽 위 대각선, 오른쪽, 오른쪽 아래 대각선
+int dx[] = {-1, 0, 1};
+int dy[] = {1, 1, 1};
+
+int dfs(int x, int y) {
+
+    visited[x][y] = 1;
+    if (y == m-1) {
+        answer++;
+        return 1;
+    }
+
+    for (int i=0; i<3; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if (visited[nx][ny] || map[nx][ny] == 'x') continue;
+
+
+        if (dfs(nx, ny))
+            return 1;
+
+    }
+    return 0;
 }
- 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
- 
-	cin >> r >> c;
-	
-	for (int i = 0; i < r; ++i)
-		cin >> map[i];
- 
-	for (int i = 0; i < r; ++i)
-		dfs(i, 0);
-	
-	cout << ret;
- 
-	return 0;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+            cin >> map[i];
+    }
+    memset(visited, 0, sizeof(visited));
+    for (int i=0; i<n; i++) {
+        dfs(i, 0);
+    }
+    cout << answer;
+
+    return 0;
 }
